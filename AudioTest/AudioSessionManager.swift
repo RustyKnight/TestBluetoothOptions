0d95eeb237dbc7efc74f21b72a595c74a951e024
Shane Whitehead <shane.whitehead@beamcommunications.com>
2016-09-14 17:19:32 +1000

    Initial commit

---
+++ AudioTest/AudioSessionManager.swift
@@ -0,0 +1,642 @@
+//
+//  AudioSessionManager.swift
+//  AudioTest
+//
+//  Created by Shane Whitehead on 13/09/2016.
+//  Copyright © 2016 Beam Communications. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import AVFoundation
+
+enum AudioSessionManagerError: Error {
+	case noInputsAvailable
+}
+
+/*
+Try and remember inputs is audio coming in, output is audio going out, I think :P
+*/
+
+protocol AudioRouteSelectable {
+	var route: AudioRoute? {get}
+	var portOverride: AVAudioSessionPortOverride? {get}
+}
+
+protocol AudioRouteSelectionErrorable: AudioRouteSelectable {
+	var error: Error {get}
+}
+
+struct AudioRouteSelectionError: AudioRouteSelectionErrorable, CustomStringConvertible {
+	let error: Error
+	let route: AudioRoute?
+	let portOverride: AVAudioSessionPortOverride?
+	
+	init(error: Error, route: AudioRoute? = nil, portOverride: AVAudioSessionPortOverride? = nil) {
+		self.error = error
+		self.route = route
+		self.portOverride = portOverride
+	}
+	
+	var description: String {
+		var message = "AudioRouteSelectionError: "
+		if let route = route {
+			message += "Route = [\(route)]"
+		} else if let portOverride = portOverride {
+			message += "Port Override = [\(portOverride)]"
+		}
+		message += "; \(error)"
+		
+		return message
+	}
+}
+
+struct AudioRouteSelection: AudioRouteSelectable, CustomStringConvertible {
+	let route: AudioRoute?
+	let portOverride: AVAudioSessionPortOverride?
+	
+	init(route: AudioRoute? = nil, portOverride: AVAudioSessionPortOverride? = nil) {
+		self.route = route
+		self.portOverride = portOverride
+	}
+	
+	var description: String {
+		var message = "AudioRouteSelection: "
+		if let route = route {
+			message += "Route = [\(route)]"
+		} else if let portOverride = portOverride {
+			message += "Port Override = [\(portOverride)]"
+		}
+		
+		return message
+	}
+}
+
+typealias AudioRouteSelectionCancelled = () -> Void
+typealias AudioRouteSelected = (AudioRouteSelectable) -> Void
+typealias AudioRouteSelectionErrored = (AudioRouteSelectionErrorable) -> Void
+
+struct AudioRoute: CustomStringConvertible, Hashable {
+
+	var inputRoute: AVAudioSessionPortDescription?
+	var outputRoute: AVAudioSessionPortDescription?
+	
+	init(outputRoute: AVAudioSessionPortDescription? = nil, inputRoute: AVAudioSessionPortDescription? = nil) {
+		self.inputRoute = inputRoute
+		self.outputRoute = outputRoute
+	}
+	
+	var portName: String {
+		if let outputRoute = outputRoute {
+			return outputRoute.portName
+		} else if let inputRoute = inputRoute {
+			return inputRoute.portName
+		}
+		return "Unknown"
+	}
+	
+	var portType: String {
+		if let outputRoute = outputRoute {
+			return outputRoute.portType
+		} else if let inputRoute = inputRoute {
+			return inputRoute.portType
+		}
+		return "Unknown"
+	}
+	
+	var description: String {
+		return "AudioRoute: [\(portName)] of [\(portType)] type"
+	}
+	
+	var hashValue: Int {
+		return "\(portName),\(portType)".hashValue
+	}
+	
+	static func ==(lhs: AudioRoute, rhs: AudioRoute) -> Bool {
+		return lhs.portName == rhs.portName && lhs.portType == rhs.portType
+	}
+}
+
+extension AVAudioSessionRouteChangeReason : CustomStringConvertible {
+	
+	public var description: String {
+		switch  self {
+		case .unknown: return "Unknown"
+		case .newDeviceAvailable: return "newDeviceAvilable"
+		case .oldDeviceUnavailable: return "oldDeviceUnabailble"
+		case .categoryChange: return "CategoryChanged"
+		case .override: return "Override"
+		case .wakeFromSleep: return "wakeFromSleep"
+		case .noSuitableRouteForCategory: return "No Suitable Route for Category"
+		case .routeConfigurationChange: return "Route Configuration Changed"
+		}
+	}
+}
+
+extension AVAudioSessionPortOverride: CustomStringConvertible {
+	public var description: String {
+		switch self {
+		case .none: return "None"
+		case .speaker: return "Speaker"
+		}
+	}
+}
+
+class AudioSessionManager: NSObject {
+	static let shared = AudioSessionManager()
+
+	let session: AVAudioSession = AVAudioSession.sharedInstance()
+	var availableRoutes: [AudioRoute] = []
+	
+	override init() {
+		monitor = false
+		
+		super.init()
+		
+		do {
+			try session.setActive(false)
+			try applyDefaults(with: [.allowBluetooth, .defaultToSpeaker])
+			availableRoutes = try audioRoutes()
+			try session.setActive(false)
+			try configure(withOverride: .speaker)
+			try session.setActive(true)
+		} catch let error {
+			print(error)
+		}
+		
+	}
+	
+	func applyDefaults(with: [AVAudioSessionCategoryOptions] = []) throws {
+		var baseOptions: AVAudioSessionCategoryOptions = [.mixWithOthers]
+		for option in with {
+			baseOptions.insert(option)
+		}
+		try session.setCategory(AVAudioSessionCategoryPlayAndRecord, with: baseOptions)
+		try session.overrideOutputAudioPort(.speaker)
+		try session.setMode(AVAudioSessionModeVoiceChat)
+	}
+	
+	func requestRecordPermission(_ block: @escaping PermissionBlock) {
+		session.requestRecordPermission(block)
+	}
+	
+	var monitor: Bool {
+		didSet {
+			print("monitor: \(monitor)")
+			if monitor {
+				NotificationCenter.default.addObserver(self,
+				                                       selector: #selector(AudioSessionManager.routeChanged),
+				                                       name: NSNotification.Name.AVAudioSessionRouteChange,
+				                                       object: nil)
+			} else {
+				NotificationCenter.default.removeObserver(self)
+			}
+		}
+	}
+	
+	func invalidateAndRefresh() throws {
+		availableRoutes = try audioRoutes()
+	}
+	
+	func routeChanged(_ notification: Notification) {
+		print("RouteChanged")
+		guard let userInfo = notification.userInfo else {
+			print("!! No userInfo?")
+			return
+		}
+		guard let reasonValue = userInfo[AVAudioSessionRouteChangeReasonKey] as? UInt else {
+			print("!! No reason?")
+			return
+		}
+		guard let reason = AVAudioSessionRouteChangeReason(rawValue: reasonValue) else {
+			print("!! Invalid reason value?")
+			return
+		}
+		
+		print(">> RouteChanged because \(reason)")
+		
+		let oldRoute = userInfo[AVAudioSessionRouteChangePreviousRouteKey] as? AVAudioSessionRouteDescription
+		
+		// :P
+		let oldOutput = oldRoute?.outputs.first?.portType
+		
+		let newRoute = session.currentRoute
+		let newOutput = newRoute.outputs.first?.portType
+		
+		print("Old = \(oldRoute?.outputs.first)")
+		print("New = \(newRoute.outputs.first)")
+		
+		print(">> oldOutput = \(oldOutput)")
+		print(">> newOutput = \(newOutput)")
+		
+		switch reason {
+		case .oldDeviceUnavailable:
+			if oldOutput == AVAudioSessionPortHeadphones {
+				// Special Scenario:
+				// when headphones are plugged in before the call and plugged out during the call
+				// route will change to {input: MicrophoneBuiltIn, output: Receiver}
+				// manually refresh session and support all devices again.
+				do {
+					try session.setActive(false)
+					try applyDefaults(with: [.allowBluetooth])
+					try session.setActive(true)
+				} catch let error {
+					print("!! Unable to update from headphones: \(error)")
+				}
+			}
+			break
+		case .newDeviceAvailable:
+			if isBluetoothDevice(port: newOutput!) {
+				print(">> Switched to bluetooth!")
+			} else if newOutput == AVAudioSessionPortHeadphones {
+				print(">> Switched to headphones!")
+			} else {
+				print(">> Switched to [\(newOutput)]")
+			}
+			break
+		case .override:
+			if isBluetoothDevice(port: oldOutput!) {
+				guard let inputs = session.availableInputs else {
+					return
+				}
+				for input in inputs {
+					print("...[\(input.portType)]")
+					if isBluetoothDevice(port: input.portType) {
+						print(">> Bluetooth is available in current inputs")
+					}
+				}
+			}
+			break
+		case .routeConfigurationChange:
+			if isBluetoothDevice(port: oldOutput!) {
+				var hasBluetooth = false
+				guard let inputs = session.availableInputs else {
+					print("!! Not available inputs")
+					return
+				}
+				
+				for input in inputs {
+					if isBluetoothDevice(port: input.portType) {
+						hasBluetooth = true
+						break
+					}
+				}
+				
+				print(">> hasBluetooth = \(hasBluetooth)")
+			}
+			break
+		default:
+			print(">> Some other reason: \(reason)")
+			break
+		}
+	}
+	
+	func isBluetoothDevice(port: String) -> Bool {
+		return port == AVAudioSessionPortBluetoothA2DP
+			|| port == AVAudioSessionPortBluetoothHFP
+	}
+	
+	fileprivate func audioRoutes() throws -> [AudioRoute] {
+		var routes: [String: AudioRoute] = [:]
+		
+		if let inputs = session.availableInputs {
+			for input in inputs {
+				print(input)
+			}
+		}
+		if let sources = session.inputDataSources {
+			for input in sources {
+				print(input)
+			}
+		}
+		if let sources = session.outputDataSources {
+			for input in sources {
+				print(input)
+			}
+		}
+		
+		routes += try audioRoutes(with: [.allowBluetooth, .defaultToSpeaker])
+		
+		return routes.values.sorted(by: { (first, second) -> Bool in
+			first.portName > second.portName
+		})
+	}
+	
+	fileprivate func audioRoutes(`for` port: AVAudioSessionPortOverride? = nil,
+	                             with options: [AVAudioSessionCategoryOptions] = []) throws -> [String: AudioRoute] {
+		var routes: [String: AudioRoute] = [:]
+		
+		print("For \(port)")
+		
+		// Cycle the audio session in order to update the available
+		// inputs/outputs
+		try session.setActive(false)
+		
+		try applyDefaults(with: options)
+		if let port = port {
+			try configure(withOverride: port)
+		}
+		
+		try session.setActive(true)
+		
+		let currentRoute = session.currentRoute
+		for output in currentRoute.outputs {
+			print(">> Output: \(output)")
+			routes[output.portName] = AudioRoute(outputRoute: output)
+		}
+		if let inputs = session.availableInputs {
+			for input in inputs {
+				print(">> Input: \(input)")
+				var route: AudioRoute!
+				if routes[input.portName] != nil {
+					route = routes[input.portName]!
+					route.inputRoute = input
+				} else {
+					route = AudioRoute(inputRoute: input)
+				}
+				routes[input.portName] = route
+			}
+		}
+		return routes
+	}
+	
+	var hasBluetooth: Bool {
+		for route in availableRoutes {
+			if isBluetoothDevice(port: route.portType) {
+				return true
+			}
+		}
+		return false
+	}
+	
+	var hasHeadset: Bool {
+		for route in availableRoutes {
+			if route.portType == AVAudioSessionPortHeadsetMic || route.portType == AVAudioSessionPortHeadphones {
+				return true
+			}
+		}
+		return false
+	}
+	
+//	func detectAvailableDevices() throws {
+//		let session = AVAudioSession.sharedInstance()
+//		
+//		// Cycle the audio session in order to update the available
+//		// inputs/outputs
+//		try session.setActive(false)
+//		try session.setActive(true)
+//		
+//		try session.setCategory(AVAudioSessionCategoryPlayAndRecord,
+//		                        with: AVAudioSessionCategoryOptions.allowBluetooth)
+//		
+//		/* input port types */
+//		print("AVAudioSessionPortLineIn = \(AVAudioSessionPortLineIn)")
+//		print("AVAudioSessionPortBuiltInMic = \(AVAudioSessionPortBuiltInMic)")
+//		print("AVAudioSessionPortHeadsetMic = \(AVAudioSessionPortHeadsetMic)")
+//		
+//		/* output port types */
+//		print("AVAudioSessionPortLineOut = \(AVAudioSessionPortLineOut)")
+//		print("AVAudioSessionPortHeadphones = \(AVAudioSessionPortHeadphones)")
+//		print("AVAudioSessionPortBluetoothA2DP = \(AVAudioSessionPortBluetoothA2DP)")
+//		print("AVAudioSessionPortBuiltInReceiver = \(AVAudioSessionPortBuiltInReceiver)")
+//		print("AVAudioSessionPortBuiltInSpeaker = \(AVAudioSessionPortBuiltInSpeaker)")
+//		print("AVAudioSessionPortHDMI = \(AVAudioSessionPortHDMI)")
+//		print("AVAudioSessionPortAirPlay = \(AVAudioSessionPortAirPlay)")
+//		print("AVAudioSessionPortBluetoothLE = \(AVAudioSessionPortBluetoothLE)")
+//		
+//		/* port types that refer to either input or output */
+//		print("AVAudioSessionPortBluetoothHFP = \(AVAudioSessionPortBluetoothHFP)")
+//		print("AVAudioSessionPortUSBAudio = \(AVAudioSessionPortUSBAudio)")
+//		print("AVAudioSessionPortCarAudio = \(AVAudioSessionPortCarAudio)")
+//		
+//		print(">> Outputs")
+//		let currentRoute = session.currentRoute
+//		for output in currentRoute.outputs {
+//			print("Output portType = \(output.portType); portName = \(output.portName)")
+//			print(" ... \(output.selectedDataSource)")
+////			if output.portType == AVAudioSessionPortHeadphones {
+////				print("output is headphones")
+////			} else if output.portType == AVAudioSessionPortBluetoothA2DP
+////				|| output.portType == AVAudioSessionPortBluetoothHFP {
+////				print("output is bluetooth")
+////			}
+//		}
+//		
+//		print("\n>> Inputs")
+//		if let inputs = session.availableInputs {
+//			for input in inputs {
+//				print("Input portType = \(input.portType); portName = \(input.portName)")
+//				print(" ... \(input.selectedDataSource)")
+////				if input.portType == AVAudioSessionPortBluetoothA2DP
+////					|| input.portType == AVAudioSessionPortBluetoothHFP {
+////					print("input is bluetooth")
+////				}
+//			}
+//		}
+//	}
+	
+//	func configureAudioSession() throws {
+//		let session = AVAudioSession.sharedInstance()
+//		
+//		try session.setActive(false)
+//		
+//		// Does the device have any input types (microphone)
+//		guard let _ = session.availableInputs else {
+//			throw AudioSessionManagerError.noInputsAvailable
+//		}
+//		
+//		// PlayAndRecord in order to redirect output audio
+//		let category = AVAudioSessionCategoryPlayAndRecord
+//		try session.setCategory(category, with: AVAudioSessionCategoryOptions.allowBluetooth)
+//		
+//		try session.setActive(true)
+//		
+//		try session.overrideOutputAudioPort(AVAudioSessionPortOverride.speaker)
+//	}
+	
+	func configure(withOverride override: AVAudioSessionPortOverride) throws {
+		
+		try session.setActive(false)
+		
+		// Does the device have any input types (microphone)
+		guard let _ = session.availableInputs else {
+			throw AudioSessionManagerError.noInputsAvailable
+		}
+		
+		// PlayAndRecord in order to redirect output audio
+		try applyDefaults()
+
+		try session.setActive(true)
+		
+		print("Override with \(override)")
+		try session.overrideOutputAudioPort(override)
+	}
+	
+	func configure(withRoute route: AudioRoute) throws {
+		
+		try session.setActive(false)
+		
+		// Does the device have any input types (microphone)
+		guard let _ = session.availableInputs else {
+			throw AudioSessionManagerError.noInputsAvailable
+		}
+		
+		if isBluetoothDevice(port: route.portName) {
+			try applyDefaults(with: [.allowBluetooth])
+		} else {
+			try applyDefaults()
+		}
+		
+		if let inputRoute = route.inputRoute {
+			print("Set inputRoute to \(inputRoute)")
+			try session.setPreferredInput(route.inputRoute)
+//			try session.setInputDataSource(inputRoute.preferredDataSource)
+		}
+		if let outputRoute = route.outputRoute {
+			print("Set outputRoute to \(outputRoute)")
+//			try session.setPreferredInput(route.inputRoute)
+			try session.setOutputDataSource(outputRoute.preferredDataSource)
+		}
+		
+		try session.setActive(true)
+	}
+	
+	var currentInput: AVAudioSessionPortDescription? {
+		let currentRoute = session.currentRoute
+		return currentRoute.inputs.first
+	}
+	var currentOutput: AVAudioSessionPortDescription? {
+		let currentRoute = session.currentRoute
+		return currentRoute.outputs.first
+	}
+	
+	func audioAlertActions(selected: AudioRouteSelected? = nil, errored: AudioRouteSelectionErrored? = nil) -> [UIAlertAction] {
+		var actions: [UIAlertAction] = []
+		
+		let input = currentInput
+		let output = currentOutput
+		
+		print("Current set to \(input); \(output)")
+		
+		var checked: Bool = false
+		let routes = availableRoutes
+		let filteredRoutes = routes.filter({ (route) -> Bool in
+			return route.portType != AVAudioSessionPortBuiltInMic &&
+				route.portType != AVAudioSessionPortBuiltInSpeaker &&
+				route.portType != AVAudioSessionPortBuiltInReceiver
+		})
+		
+		for route in filteredRoutes {
+			let action = UIAlertAction(title: route.portName, style: .default, handler: { (action) in
+				do {
+					try AudioSessionManager.shared.configure(withRoute: route)
+					selected?(AudioRouteSelection(route: route))
+				} catch let error {
+					errored?(AudioRouteSelectionError(error: error, route: route))
+				}
+			})
+			if AudioSessionManager.shared.isBluetoothDevice(port: route.portType) {
+				action.setValue(#imageLiteral(resourceName: "blueTooth"), forKey: "image")
+			}
+			if route.portName == input?.portName {
+				checked = true
+				action.setValue(true, forKey: "checked")
+			}
+			
+			actions.append(action)
+		}
+		
+		let model = UIDevice.current.model
+		let phoneAction = UIAlertAction(title: model, style: .default, handler: { (action) in
+			do {
+				try AudioSessionManager.shared.configure(withOverride: AVAudioSessionPortOverride.none)
+				selected?(AudioRouteSelection(portOverride: .none))
+			} catch let error {
+				print("!! Error \(error)")
+				errored?(AudioRouteSelectionError(error: error, portOverride: .none))
+			}
+		})
+		if !checked && AVAudioSessionPortBuiltInReceiver == output?.portName {
+			phoneAction.setValue(true, forKey: "checked")
+		}
+		actions.append(phoneAction)
+
+		let speakerAction = UIAlertAction(title: "Speaker", style: .default, handler: { (action) in
+			do {
+				try AudioSessionManager.shared.configure(withOverride: AVAudioSessionPortOverride.speaker)
+				selected?(AudioRouteSelection(portOverride: .speaker))
+			} catch let error {
+				errored?(AudioRouteSelectionError(error: error, portOverride: .speaker))
+			}
+		})
+//		AVAudioSessionPortBuiltInReceiver
+		if !checked && AVAudioSessionPortBuiltInSpeaker == output?.portName {
+			speakerAction.setValue(true, forKey: "checked")
+		}
+		speakerAction.setValue(#imageLiteral(resourceName: "speaker"), forKey: "image")
+		actions.append(speakerAction)
+		
+		return actions
+	}
+	
+	func alertController(selected: AudioRouteSelected? = nil, errored: AudioRouteSelectionErrored? = nil, cancelled: AudioRouteSelectionCancelled? = nil) -> UIAlertController {
+		let controller = UIAlertController(title: nil,
+		                                   message: nil,
+		                                   preferredStyle: UIAlertControllerStyle.actionSheet)
+		var actions = AudioSessionManager.shared.audioAlertActions(selected: selected, errored: errored)
+		actions.append(UIAlertAction(title: "Cancel", style: .cancel, handler: {(action) in
+			cancelled?()
+		}))
+		for action in actions {
+			controller.addAction(action)
+		}
+		return controller
+	}
+}
+
+/**
+newDict = leftDict + rightDict
+*/
+
+/**
+`+` operator for merging two dictionaries
+
+`newDict = leftDict + rightDict`
+
+- parameter left:  Dictionary to be merged
+- parameter right: Dictionary to be merged
+
+- returns: A new dictionary which is a merge of the left and right values,
+where the right will override any duplicate keys from the left
+*/
+func + <K, V> (left: [K:V], right: [K:V]) -> [K:V] {
+	var new = [K:V]()
+	for (k, v) in left {
+		new[k] = v
+	}
+	for (k, v) in right {
+		new[k] = v
+	}
+	return new
+}
+
+/**
++= operator for merging two dictionaries, inplace
+
+leftDict += rightDict
+*/
+
+/**
+`+=` operator for merging two dictionaries, inplace
+
+`leftDict += rightDict`
+
+- parameter left:  Source dictionary
+- parameter right: Dictionary to be merged
+*/
+func += <K, V> ( left: inout [K:V], right: [K:V]?) {
+	guard let right = right else { return }
+	right.forEach { key, value in
+		left.updateValue(value, forKey: key)
+	}
+}
---
+++ AudioTest/ViewController.swift
@@ -0,0 +1,206 @@
+//
+//  ViewController.swift
+//  AudioTest
+//
+//  Created by Shane Whitehead on 13/09/2016.
+//  Copyright © 2016 Beam Communications. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+import MediaPlayer
+
+extension MPMusicPlaybackState : CustomStringConvertible {
+	
+	public var description: String {
+		switch self {
+		case .stopped: return "Stopped"
+		case .playing: return "Playing"
+		case .paused: return "Paused"
+		case .interrupted: return "Interrupted"
+		case .seekingForward: return "Seeking Forward"
+		case .seekingBackward: return "Seeking Backward"
+		}
+	}
+}
+
+
+class ViewController: UIViewController {
+	
+	@IBOutlet weak var monitor: UIButton!
+	@IBOutlet weak var configure: UIButton!
+	@IBOutlet weak var detect: UIButton!
+	
+	@IBOutlet weak var hasHeadsetLabel: UILabel!
+	@IBOutlet weak var hasBluetoothLabel: UILabel!
+	
+	let player = MPMusicPlayerController.applicationMusicPlayer()
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		AudioSessionManager.shared.requestRecordPermission { (authenticated) in
+			print("Record authentication = \(authenticated)")
+		}
+		
+		doMonitor(self)
+		
+		NotificationCenter.default.addObserver(self, selector: #selector(ViewController.nowPlayingItemChanged), name: NSNotification.Name.MPMusicPlayerControllerNowPlayingItemDidChange, object: nil)
+		NotificationCenter.default.addObserver(self, selector: #selector(ViewController.playBackStateChanged), name: NSNotification.Name.MPMusicPlayerControllerPlaybackStateDidChange, object: nil)
+		
+		player.beginGeneratingPlaybackNotifications()
+		player.shuffleMode = .off
+		player.repeatMode = .all
+		
+		let predicate = MPMediaPropertyPredicate(value: "Attack of the Crab Women", forProperty: MPMediaItemPropertyTitle)
+		let query = MPMediaQuery()
+		query.addFilterPredicate(predicate)
+		
+		player.setQueue(with: query)
+		print("Is Prepared to Play \(player.isPreparedToPlay)")
+		player.prepareToPlay()
+		player.play()
+		print("Playback state \(player.playbackState)")
+		print("Now playing \(player.nowPlayingItem)")
+		//		guard let items = query.items else {
+		//			return
+		//		}
+		//		for song in items {
+		//			print(song.title)
+		//		}
+
+		updateOptions()
+	}
+	
+	func updateOptions() {
+		hasHeadsetLabel.isHidden = !AudioSessionManager.shared.hasHeadset
+		hasBluetoothLabel.isHidden = !AudioSessionManager.shared.hasBluetooth
+	}
+	
+	func nowPlayingItemChanged(_ notification: Notification) {
+		print("nowPlayingItemChanged")
+		print("Now playing \(player.nowPlayingItem)")
+	}
+	
+	func playBackStateChanged(_ notification: Notification) {
+		print("playBackStateChanged")
+		print("Playback state \(player.playbackState)")
+		if player.playbackState == .paused {
+			player.prepareToPlay()
+			player.play()
+		}
+	}
+	
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+	
+	@IBAction func doDetect(_ sender: AnyObject) {
+		do {
+			try AudioSessionManager.shared.invalidateAndRefresh()
+		} catch let error {
+			print("detectAvailableDevices failed with \(error)")
+		}
+		
+		updateOptions()
+	}
+	
+	@IBAction func doConfigure(_ sender: AnyObject) {
+		do {
+			try AudioSessionManager.shared.configure(withOverride: .none)
+		} catch let error {
+			print("configureAudioSession failed with \(error)")
+		}
+	}
+	
+	@IBAction func doMonitor(_ sender: AnyObject) {
+		AudioSessionManager.shared.monitor = !AudioSessionManager.shared.monitor
+		monitor.setTitle(AudioSessionManager.shared.monitor ? "Stop" : "Monitor", for: [])
+	}
+	
+	@IBAction func doRouter(_ sender: AnyObject) {
+		selectOutput()
+	}
+	
+	func selectOutput() {
+		//		let controller = UIAlertController(title: nil,
+		//		                                   message: nil,
+		//		                                   preferredStyle: UIAlertControllerStyle.actionSheet)
+		let controller = AudioSessionManager.shared.alertController(
+			selected: { (route) in
+				print("selected \(route)")
+				self.player.play()
+			}, errored: { (error) in
+				print("Error: ")
+				print("\(error)")
+		}) {
+			print("Cancelled")
+		}
+		
+		//		for action in actions {
+		//			controller.addAction(action)
+		//		}
+		
+		//		var speakerRouteChecked: Bool = true
+		//		do {
+		//			let routes = try AudioSessionManager.shared.audioRoutes()
+		//			let filteredRoutes = routes.filter({ (route) -> Bool in
+		//				return route.portType != AVAudioSessionPortBuiltInMic &&
+		//					route.portType != AVAudioSessionPortBuiltInSpeaker &&
+		//					route.portType != AVAudioSessionPortBuiltInReceiver
+		//			})
+		//
+		//			for route in filteredRoutes {
+		//				let action = UIAlertAction(title: route.portName, style: .default, handler: { (action) in
+		//					print(route)
+		////					if AudioSessionManager.shared.isBluetoothDevice(port: route.portType) {
+		////						do {
+		////							try session.setCategory(AVAudioSessionCategoryPlayAndRecord, with: .allowBluetooth)
+		////						} catch let error {
+		////							print("!! Error \(error)")
+		////						}
+		////					}
+		//				})
+		//				if AudioSessionManager.shared.isBluetoothDevice(port: route.portType) {
+		//					action.setValue(#imageLiteral(resourceName: "blueTooth"), forKey: "image")
+		//				}
+		//				if route.isCurrentRoute {
+		//					speakerRouteChecked = false
+		//					action.setValue(true, forKey: "checked")
+		//				}
+		//				controller.addAction(action)
+		//			}
+		//		} catch let error {
+		//			print("Failed to list audio routes:  \(error)")
+		//		}
+		//
+		//		let model = UIDevice.current.model
+		//		let phoneAction = UIAlertAction(title: model, style: .default, handler: { (action) in
+		//			do {
+		//				try session.setCategory(AVAudioSessionCategoryPlayAndRecord)
+		//				try session.overrideOutputAudioPort(AVAudioSessionPortOverride.none)
+		//			} catch let error {
+		//				print("!! Error \(error)")
+		//			}
+		//		})
+		//		if speakerRouteChecked {
+		//			phoneAction.setValue(true, forKey: "checked")
+		//		}
+		//		controller.addAction(phoneAction)
+		//
+		//		let speakerAction = UIAlertAction(title: "Speaker", style: .default, handler: { (action) in
+		//			do {
+		//				try session.setCategory(AVAudioSessionCategoryPlayAndRecord)
+		//				try session.overrideOutputAudioPort(AVAudioSessionPortOverride.speaker)
+		//			} catch let error {
+		//				print("!! Error \(error)")
+		//			}
+		//		})
+		//		speakerAction.setValue(#imageLiteral(resourceName: "speaker"), forKey: "image")
+		//		controller.addAction(speakerAction)
+		//
+		//		controller.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+		
+		present(controller, animated: true, completion: nil)
+	}
+}
